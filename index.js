01263a12a30dba0611f96d8368d7f442bf03c732
Shukri Abdi <shukri751@gmail.com>
2024-07-13 01:30:05 +0300

    phase-0-the-dom-modifying-elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+// Remove the <main> element with id 'main'
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// Set the id of the new <h1> element to 'victory'
+newHeader.id = 'victory';
+
+// Set the text content of the new <h1> element
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+// Append the new <h1> element to the body
+document.body.appendChild(newHeader);
+
